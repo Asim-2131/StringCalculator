6d530662a00bdcaa9f6216b9facfad1f735b14a9
Asim Vora <asimvora2001@gmail.com>
2021-08-27 15:32:04 +0530

    Adding Comments for better understanding

--- src/SC/StringCalc.java
+++ src/SC/StringCalc.java
@@ -9,17 +9,22 @@ import java.util.regex.Pattern;
 public class StringCalc{
 
 
+	// Main Add Function
+	
 	public static int Add(String s) {
 		if(s.isEmpty()) return 0;
 		else if(s.length() == 1) return sToInt(s);
 		else{
 			if(s.startsWith("//"))
 				return customDelimiters(s);
-			return separateCaseForCommaAndNewLineCombine(s);
+			else
+				return separateCaseForCommaAndNewLineCombine(s);
 		}
 	}
 
-
+	
+	// Function For custom Delimiters
+	
 	private static int customDelimiters(String s) {
 		ArrayList<String>delimiters = new ArrayList<String>();
 		String [] refString = s.split("\n");
@@ -32,6 +37,7 @@ public class StringCalc{
 		return additionOfString(numbers);
 	}
 	
+	// extracting delimiters from string
 	
 	private static ArrayList<String> differentDelimiters(String s){
 		Pattern regex = Pattern.compile("\\[[*%$,@#^]*\\]");
@@ -45,17 +51,21 @@ public class StringCalc{
 		return a;
 	}
 	
+	// Case for comma and new line
 	
 	private static int separateCaseForCommaAndNewLineCombine(String s) {
 		String numbers [] = s.split(",|\n");
 		return additionOfString(numbers);
 	}
 
+	// Function for splitting string after new line with following passed string in split function
 	
 	private static String[] splitByDelimiters(String s,ArrayList<String>delimiters) {
 		return s.split(combinationOfSplitingDelimiters(delimiters));
 	}
 	
+	
+	// Make string of string  for fit into split function 
 	
 	private static String combinationOfSplitingDelimiters(ArrayList<String>delimiters) {
 		String ref="";
@@ -68,6 +78,7 @@ public class StringCalc{
 		return ref;
 	}
 	
+	// Make String from characters to separate delimiters
 	
 	private static String splitFormForParticularPattern(String s) {
 		String ref = "";
@@ -77,6 +88,7 @@ public class StringCalc{
 		return ref;
 	}
 	
+	// Addition of numbers extracted from string after processing string
 	
 	private static int additionOfString(String numbers[]) {
 		int val = 0;
@@ -86,11 +98,12 @@ public class StringCalc{
 			val += (sToInt(numbers[i]) <= 1000 ? sToInt(numbers[i]) : 0);
 		}
 		if(negative.size() > 0) {
-			throw new RuntimeException("negative not allowed " + combineNgNumbers(negative));
+			throw new IllegalArgumentException("negative not allowed " + combineNgNumbers(negative));
 		}
 		return val;
 	}
 	
+	// Combine negative numbers as a string for passing in Exception
 	
 	private static String combineNgNumbers(ArrayList<Integer> negative) {
 		String ref = "";
@@ -101,6 +114,7 @@ public class StringCalc{
 		return ref;
 	}
 
+	// String to integer Converter
 
 	private static int sToInt(String s) {
 		return Integer.parseInt(s);
